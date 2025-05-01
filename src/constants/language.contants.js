d8135d1259c7a67f54885505a7c157bccc79d376
Janardhan Majumder <janardhan.md03@gmail.com>
2025-05-01 16:38:41 +0600

    add localization

--- src/constants/language.contants.js
+++ src/constants/language.contants.js
@@ -2,6 +2,6 @@ export const supportedLanguages = [
   { title: "English", name: "en" },
   { title: "Português", name: "pt" },
   { title: "বাংলা", name: "bn" },
-  { title: "العربية", name: "ar" },
+  // { title: "العربية", name: "ar" },
   { title: "Français", name: "fr" },
 ];
